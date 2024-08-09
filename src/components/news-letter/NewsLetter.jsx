9b3a64d3873e641f74a426dd9f6847fa70787dfd
minhajgrohance <minhaj.grohance@gmail.com>
2024-08-09 18:17:53 +0530

    news letter changed

--- src/components/news-letter/NewsLetter.jsx
+++ src/components/news-letter/NewsLetter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaWhatsapp } from 'react-icons/fa'
+import { FaWhatsapp, FaInstagram } from 'react-icons/fa'
 
 //imports................................................................................................
 
@@ -8,17 +8,21 @@ function NewsLetter() {
 		<div className='bg-[#88141E] hidden items-center gap-2 p-16 text-white md:flex flex-col'>
 			<h1 className='capitalize text-xl'>stay updated</h1>
 			<h1 className='text-3xl text-center capitalize'>
-				join our whatsapp community
+				Follow Us on Instagram
 			</h1>
 			<p className='text-center'>
-				Get the latest offers, updates, and exclusive deals. <br />
-				Join our WhatsApp community and stay connected!
+				Get the latest offers, updates, and exclusive deals. Follow our
+				Instagram page and stay connected!
 			</p>
 			<form className='pt-3'>
-				<button className='bg-white flex justify-center items-center gap-2 font-semibold capitalize py-2 px-4 text-[#88141E]  rounded-full'>
-					<FaWhatsapp className='text-green-600' />
+				<a
+					href='https://www.instagram.com/vayko.in/'
+					target='_blank'
+					className='bg-white flex justify-center items-center gap-2 font-semibold capitalize py-2 px-4 text-[#88141E]  rounded-full'
+				>
+					<FaInstagram className='text-purple-500' />
 					join now
-				</button>
+				</a>
 			</form>
 		</div>
 	)
--- src/components/social-media/InstaCard.jsx
+++ src/components/social-media/InstaCard.jsx
@@ -4,7 +4,7 @@ function InstaCard({ embed }) {
 	return (
 		<div
 			dangerouslySetInnerHTML={{ __html: embed }}
-			className='relative shadow-lg w-[104px] md:w-[204px] md:h-[350px] h-[175px] flex justify-center items-center rounded-lg overflow-hidden'
+			// className='relative shadow-lg w-[104px]  md:w-[204px] md:h-[350px] h-[175px] flex justify-center items-center rounded-lg overflow-hidden'
 		></div>
 	)
 }
