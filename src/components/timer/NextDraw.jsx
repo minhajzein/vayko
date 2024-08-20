a9fd01124058a62d840595de35609476b4d2832e
minhajgrohance <minhaj.grohance@gmail.com>
2024-08-20 09:48:24 +0530

    padding top for running letters

--- src/components/timer/NextDraw.jsx
+++ src/components/timer/NextDraw.jsx
@@ -5,7 +5,7 @@ import { FaStarOfLife } from 'react-icons/fa'
 
 function NextDraw() {
 	return (
-		<div className='md:px-[80px] font-silkscreen text-xs md:text-3xl pb-2 overflow-hidden capitalize w-full'>
+		<div className='md:px-[80px] pt-5 font-silkscreen text-xs md:text-3xl pb-2 overflow-hidden capitalize w-full'>
 			<div className='w-auto grid grid-flow-col'>
 				<div className='w-screen flex justify-evenly gap-5 animate-infinite-carousel'>
 					<h1 className='w-full'>
